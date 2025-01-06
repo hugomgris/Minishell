905d29e78b30165eba8d9e201605ee287dfb29b3
nponchon <nponchon@student.42.fr>
2025-01-06 10:26:46 +0100

    "||" now is recognised as a valid token

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: nponchon <nponchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 11:19:44 by hmunoz-g          #+#    #+#             */
-/*   Updated: 2024/12/23 17:01:26 by nponchon         ###   ########.fr       */
+/*   Updated: 2025/01/06 10:24:28 by nponchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -134,7 +134,6 @@ int	ms_parser(t_ms *ms, char *str)
 	ms_remove_quotes(ms);
 	ms_remove_empty_tokens(&ms->tok, free);
 	ms_sort_toks(ms->tok);
-	//ms_print_toks(ms->tok);
 	ms_copy_toktolist(ms);
 	ms_tokclear(&ms->tok, free);
 	return (TRUE);
