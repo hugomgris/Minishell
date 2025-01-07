47f5898ee57a45b01b32a22bff63349e2bf12501
Nicolas Ponchon <nponchon@student.42barcelona.com>
2025-01-07 21:34:56 +0100

    couple of things to correct added in redirection.c

--- src/executor/redirection.c
+++ src/executor/redirection.c
@@ -11,7 +11,11 @@
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
+//! TODO
+// cat << EOF | echo kikoooulol >> outfile | sleep 5 | wc -w > outfile2
+// -> yields overflow
+// echo 'hola | adios'
+// echo should not be printed
 /*
 Helper function for input redirection (<).
 Gets the latest value (file) for the redirection in the input.
