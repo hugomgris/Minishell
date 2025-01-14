56689de13e6336c872a6456519eb4afc9298f981
Hugo Munoz Gris <hmunoz-g@car13s4.42barcelona.com>
2025-01-14 10:13:02 +0100

    executor.c

---
+++ .dep/executor/piping.d
@@ -0,0 +1,13 @@
+.obj/executor/piping.o: src/executor/piping.c \
+  src/executor/../../includes/minishell.h \
+  src/executor/../../includes/../libs/libft/libft.h \
+  src/executor/../../includes/../libs/libft/ft_printf/includes/ft_printf.h \
+  src/executor/../../includes/../libs/libft/ft_printf/includes/../../libft.h
+
+src/executor/../../includes/minishell.h:
+
+src/executor/../../includes/../libs/libft/libft.h:
+
+src/executor/../../includes/../libs/libft/ft_printf/includes/ft_printf.h:
+
+src/executor/../../includes/../libs/libft/ft_printf/includes/../../libft.h:
